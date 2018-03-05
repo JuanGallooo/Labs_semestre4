ef4042bfa93faf9dbbb788a876cbb27daa66acae
manuelq12 <manuelq12@users.noreply.github.com>
2018-03-05 13:32:59 -0500

    hash problem

--- src/datos/HashTable.java
+++ src/datos/HashTable.java
@@ -4,32 +4,19 @@ import interfaces.interfazHash;
 import mundo.Carro;
 
 public class HashTable implements interfazHash<Carro>{
-	
-	public int[] tableBahias;
+
+	// Este arreglo debe ser de carros?
 	public String[] tableCarros;
 	
 	public HashTable(int tamanhoUniverso){
-		 
+		 if(tamanhoUniverso > 100){
+			 tableCarros = new String [375];
+		 }
+		 else{
+			 tableCarros= new String[100];
+		 }
 	}
 	
-	
-	@Override
-	public int keySet(Carro object) {
-	
-    char[] placa = object.getPlaca().toCharArray();
-    int hash = 0;
-    
-    for(int i  = 0; i < placa.length; i++){
-    	if(placa[i]>= 48 && placa[i]<=57){
-    		hash+= Math.abs(placa[i]-57);
-    	}
-    	else{
-    		hash+= Math.abs(placa[i]-90);
-    	}
-    }
-		
-		return hash;
-	}
 
 	@Override
 	public Carro replace(Carro object) {
@@ -46,8 +33,18 @@ public class HashTable implements interfazHash<Carro>{
 		return false;
 	}
 
+	// Debo recibir la placa no? en vez del carro?.
 	@Override
-	public Carro Get(String key) {
+	public Carro Get(Carro placa) {
+		int key = 0;
+		if(this.size() > 100){
+			key = hashFunctionCarsBigger100(placa);
+		}
+		else{
+			key = hashFunctionCarsLess100(placa);
+		}
+		// Retorno el string del carro o retorno el carro?
+		
 		return null;
 	}
 
@@ -58,7 +55,48 @@ public class HashTable implements interfazHash<Carro>{
 
 	@Override
 	public int size() {
-		return 0;
+		return tableCarros.length;
 	}
+
+
+	// No recibo más bien una placa para hacer una referencia del carro?
+	//---------------------------------------------------------------------------------------------------------------------
+	@Override
+	public int hashFunctionCarsBigger100(Carro object) {	
+	    char[] placa = object.getPlaca().toCharArray();
+	    int hash = 0;
+	    
+	    for(int i  = 0; i < placa.length; i++){
+	    	if(placa[i]>= 48 && placa[i]<=57){
+	    		hash+= Math.abs(placa[i]-57);
+	    	}
+	    	else{
+	    		hash+= Math.abs(placa[i]-90)*Math.pow(2,i);
+	    	}
+	    }
+			
+			return hash;
+	}
+
+
+	@Override
+	public int hashFunctionCarsLess100(Carro object) {
+		
+	    char[] placa = object.getPlaca().toCharArray();
+	    int hash = 0;
+	    
+	    for(int i  = 0; i < placa.length; i++){
+	    	if(placa[i]>= 48 && placa[i]<=57){
+	    		hash+= Math.abs(placa[i]-57);
+	    	}
+	    	else{
+	    		hash+= Math.abs(placa[i]-90);
+	    	}
+	    }
+			
+			return hash;
+	}
+
+
 
 }
